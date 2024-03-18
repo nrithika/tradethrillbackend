aff4aae938374e58ff6c6617d1c45474d5cdbe00
nrithika <nrithikar22@iitk.ac.in>
2024-03-18 21:51:31 +0530

    new

--- stuff/database.py
+++ stuff/database.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def make_db():
-    conn = psycopg2.connect(dbname = os.getenv("db_name"), user = os.getenv("user"), password=os.getenv("password"))
+    password = ""
+    conn = psycopg2.connect(dbname = os.getenv("db_name"), user = os.getenv("user"), password=password)
     cursor = conn.cursor()
     return (conn,cursor)
